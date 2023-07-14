a9db4d02249457820790c8282dc5c20020bd8d80
gr1fon4ik <hekjd12@gmail.com>
2023-07-14 17:51:24 +0300

    sega razvel

--- src/api/instance.ts
+++
@@ -1,3 +0,0 @@
-import axios from "axios";
-
-export const instance = axios.create({baseURL:"https://api.sbercloud.ru/content/v1/bootcamp/frontend"});
--- src/interfaces/socials.ts
+++
@@ -1,4 +0,0 @@
-export type socialItem ={
-    href:string;
-    link:string;
-}
